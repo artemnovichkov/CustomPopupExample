0e554e8ad41c4ddc93ee90afc3f5e55f3977a656
Artem Novichkov <novichkoff93@gmail.com>
2021-10-02 16:10:15 +0600

    Update view builder

--- CustomPopupExample/BottomPopupView.swift
+++ CustomPopupExample/BottomPopupView.swift
@@ -5,14 +5,18 @@
 import SwiftUI
 
 struct BottomPopupView<Content: View>: View {
-    
-    @ViewBuilder var content: () -> Content
+
+    let content: Content
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
     
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 Spacer()
-                content()
+                content
                     .padding(.bottom, geometry.safeAreaInsets.bottom)
                     .background(Color.white)
                     .cornerRadius(radius: 16, corners: [.topLeft, .topRight])
